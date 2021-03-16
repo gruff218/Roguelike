e107921645476ea01e5a5aa29c53456750866c3e
Connor Huang <connorh23@lakesideschool.org>
2021-03-16 11:42:54 -0800

    Shooting stuffs

---
+++ RoguelikeTest/Assets/Bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+            Enemy e = hitInfo.GetComponent<Enemy>();
+            if (e != null)
+            {
+                e.takeDamage(30);
+            }
+            Destroy(gameObject);
+    
+        
+        
+        
+    }
+    
+}
---
+++ RoguelikeTest/Assets/Weapon.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y+130));
+        Vector2 myPos = new Vector2(transform.position.x, transform.position.y + 1);
+        Vector2 direction = target - myPos;
+        direction.Normalize();
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
